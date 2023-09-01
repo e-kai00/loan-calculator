82ad5542bbbcc952061b5b2f8a908591c1c80b2d
Katerina <yelkaterina@gmail.com>
2023-09-01 14:18:38 +0200

    Add basic formulars

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ let paymentEveryMo = document.querySelector('#payment-mo');
 let totalInterest = document.querySelector('#interest');
 let loanInterest = document.querySelector('#loan-interest');
 let loanInput = document.querySelector('#ammount');
-let loanTermInput = document.querySelector('#term');
+let loanTermInput = document.querySelector('#years');
 let interestRateInput = document.querySelector('#rate')
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -11,14 +11,39 @@ document.addEventListener('DOMContentLoaded', () => {
     totalInterest.innerHTML = 0;
     loanInterest.innerHTML = 0;
 
+    // loanInput.value = 10000;
+    // loanTermInput.value = 3;
+    // interestRateInput.value = 5;
 
+    // calculate()
 
 });
 
-// calculate monthly payment:
-// loan principle * (interest rate / number of payments)
+function calculate() {
 
-// calculate total interest
-// (interest rate / number of payments) * loan principle = interest
+    // Convert interest from a percentage to a decimal.
+    // Convert from an annual rate to a monthly rate.
+    // Convert payment period in years to the number of monthly payments.
 
-// calculate loan + interest
+    let principal = parseFloat(loanInput.value);
+    let interest = parseFloat(interestRateInput.value) / 100 / 12;
+    let payments = parseFloat(loanTermInput.value) * 12;
+
+    // monthly payments
+    let x = Math.pow(1 + interest, payments);    
+    let monthly = (principal * x * interest) / (x - 1)
+   
+    // Check that the result is a finite number.
+    if (isFinite(monthly)) {
+        paymentEveryMo.innerHTML = monthly.toFixed(2);
+        totalInterest.innerHTML = ((monthly*payments)-principal).toFixed(2);
+        loanInterest.innerHTML = (monthly * payments).toFixed(2);
+    } else {
+        paymentEveryMo.innerHTML = "";
+        totalInterest.innerHTML = "";
+        loanInterest.innerHTML = "";
+        alert('Something went wrong. Please, try again.');
+    }
+    
+
+}
